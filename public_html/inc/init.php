bee08b91756674449e5880a6949ddf61dbdfe720
lbegnaud <lbegnaud@radersolutions.com>
2023-02-07 08:54:29 -0600

    Don't list mobs with spawnpos 0,0,0

--- public_html/inc/init.php
+++ public_html/inc/init.php
@@ -366,6 +366,7 @@ if ( isset($_POST['callback']) ) {
 											WHERE 
 												mob_groups.zoneid = ".$zone_id." AND 
 												(spawntype = 0 OR spawntype = 1 OR spawntype = 2 OR spawntype = 3 OR spawntype = 4) AND 
+												(pos_x <> 0 AND pos_y <> 0 AND pos_z <> 0) AND
 												(respawntime > 0 AND respawntime < 1800 ) AND
 												-- dropid != 0 AND
 												mob_groups.name NOT IN ( SELECT name FROM fishing_mob WHERE zoneid = ".$zone_id." ) AND
@@ -470,6 +471,7 @@ if ( isset($_POST['callback']) ) {
 												respawntime > 1800 OR
 												(spawntype = 0 AND respawntime > 1800) 
 											)
+											(pos_x <> 0 AND pos_y <> 0 AND pos_z <> 0) AND
 											AND dropid != 0 
 											-- AND ( pos_x != 0 AND pos_x != 1 )
 											AND name NOT IN ( SELECT name FROM fishing_mob WHERE zoneid = ".$zone_id." )
